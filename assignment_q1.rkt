17a7f15c640381e563dda37ad42ead9a19c41a7e
Ryanmch1997 <ryanmch1997@eircom.net>
2017-10-26 20:03:01 +0100

    Add files via upload

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,7 @@
-#lang racket
+(define partA (cons 1 2))
+(define partB (cons 1(cons 2(cons 3 '()))))
+(define partC (cons "String" (cons 2 ( cons 1 (cons 2 ( cons 3 '()))))))
+(define partD (list '(1) ' "String" ' (1 2 3)))
+(define partE (append '(String) '(1)'(1 2 3)))
+
+  
